30560b74a05eda109862a477e443a388112325d0
phablo-evocorp <phablo@evocorp.com.br>
2023-10-17 11:26:26 -0300

    updated calc for conversion bytes to mbps

--- network-io.py
+++ network-io.py
@@ -1,23 +1,22 @@
 import psutil, time, os
 
-old_bytes_recv = 0
-old_bytes_sent = 0
+def net_usage():
+   net_stats = psutil.net_io_counters()
+   net_in_1 = net_stats.bytes_recv
+   net_out_1 = net_stats.bytes_sent
+   time.sleep(1)
+   net_stats = psutil.net_io_counters()
+   net_in_2 = net_stats.bytes_recv
+   net_out_2 = net_stats.bytes_sent
+   
+   net_in = round((net_in_2 - net_in_1) * 8 / 1e6, 2)
+   net_out = round((net_out_2 - net_out_1) * 8 / 1e6, 2)
+   
+   print(f"Up: {net_out} Mbps Down: {net_in}")
+   
+   return net_in, net_out
 
 while True:
     os.system('clear')
-    
-    net_stats = psutil.net_io_counters()
-    
-    
-    print("Network bytes in: ", net_stats.bytes_recv - old_bytes_recv)
-    print("Network bytes out: ", net_stats.bytes_sent - old_bytes_sent)
-    print("Network packets send: ", net_stats.packets_sent)
-    print("Network packets receive: ", net_stats.packets_recv)
-    print("Packets error in: ", net_stats.errin)
-    print("Packets error out: ", net_stats.errout)
-    print("Drop packets in: ", net_stats.dropin)
-    print("Drop packets out: ", net_stats.dropout)
+    net_usage()
     time.sleep(1)
-    
-    old_bytes_recv = net_stats.bytes_recv
-    old_bytes_sent = net_stats.bytes_sent
